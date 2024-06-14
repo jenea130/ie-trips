04eb940430fd60ea41412296bb7cd293560a5a39
jenea130 <jenea130@mail.ru>
2024-06-14 17:49:09 +0300

    upd: gradient home-intro

--- header.php
+++ header.php
@@ -14,43 +14,19 @@
     <?php
     $header = get_field('header', 'option');
     $logo = $header['logo'];
+    $button_search = $header['button_search'];
+    $button_text = $header['button_text'];
     ?>
     <header class="main-header">
-      <div class="logo">
-        <?php if ($logo) : ?>
-          <?php if (is_front_page()) : ?>
-            <img src="<?php echo $logo; ?>" alt="">
-          <?php else : ?>
-            <a href="<?php echo home_url(); ?>">
-              <img src="<?php echo $logo; ?>" alt="">
-            </a>
-          <?php endif; ?>
-        <?php endif; ?>
-      </div>
-      <?php wp_nav_menu([
-        'theme_location'  => 'menu-1',
-        'menu'            => '',
-        'container'       => '',
-        'container_class' => '',
-        'container_id'    => '',
-        'menu_class'      => 'main-menu main-header__main-menu',
-        'menu_id'         => 'js-main-menu',
-        'echo'            => true,
-        'fallback_cb'     => 'wp_page_menu',
-        'before'          => '',
-        'after'           => '',
-        'link_before'     => '',
-        'link_after'      => '',
-        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-        'depth'           => 0,
-        'walker'          => '',
-      ]); ?>
-
-      <div class="main-header__sandwich sandwitch-wrap" id="js-sandwitch-wrap">
-        <div class="sandwitch">
-          <div class="sandwitch__line sandwitch__line--top"></div>
-          <div class="sandwitch__line sandwitch__line--middle"></div>
-          <div class="sandwitch__line sandwitch__line--bottom"></div>
+      <div class="container">
+        <div class="main-header__wrap">
+          <div class="logo">
+            <?php echo $logo ?>
+          </div>
+          <button class="search" type="button">
+            <?php echo $button_search ?>
+          </button>
+          <button class="main-header__btn" type="button"><?php echo $button_text ?></button>
         </div>
       </div>
     </header>
